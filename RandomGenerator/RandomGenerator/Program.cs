382ac1f5c992c0c648e6154e62d2889ad8fea7c4
Laura Sipiä <laura.sipia@tuni.fi>
2024-02-08 18:58:14 +0200

    RandomGenerator added

---
+++ RandomGenerator/RandomGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿static int Rand32()
+{
+    List<List<int>> possibleValues;
+    Random rnd = new();
+
+    int rand = rnd.Next(1, 5);
+
+    if (rand == 1 || rand == 2)
+    {
+        possibleValues =
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ];
+    }
+    else
+    {
+        possibleValues =
+        [
+            [17, 18, 19, 20],
+            [21, 22, 23, 24],
+            [25, 26, 27, 28],
+            [29, 30, 31, 32]
+        ];
+    }
+
+    int secondRand = rnd.Next(1, 5);
+
+    var group = possibleValues[secondRand - 1];
+
+    int thirdRand = rnd.Next(1, 5);
+
+    return group[thirdRand - 1];
+
+}
+
+
+Console.WriteLine(Rand32());
